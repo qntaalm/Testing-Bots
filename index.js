1e8802c57260ba135419044811f956fd7c6af513
qntaalm <askaik000@gmail.com>
2024-08-24 18:13:27 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+//====================================
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton, Modal, TextInputComponent,  showModal, InteractionCollector, SelectMenuComponent, MessageSelectMenu, PermissionFlagsBits } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.MESSAGE_CONTENT] });
+//====================================
+
+
+client.on('ready', () => {
+  console.clear()
+console.log(`${client.user.tag} is Online 🟢`);
+    client.user.setStatus("idle")
+    let status =
+        [
+        `${client.user.username} System`,
+        `My Dev M7md6565`,
+        `My Prefix ${prefix} | Mention Me`,
+        ]
+    setInterval(()=>{
+        client.user.setActivity(status[Math.floor(Math.random()*status.length)]);
+        },8000)
+});
+
+process.on("uncaughtException" , err => {
+  return console.log(err)
+});
+
+process.on("unhandledRejection" , err => {
+  return console.log(err)
+});
+
+process.on("rejectionHandled", err => {
+  return console.log(err)
+});
+client.login(process.env.token)
+
+client.once('ready', () => {
+  const targetChannelId = '1276922235260633212';
+  const targetChannel = client.channels.cache.get(targetChannelId);
+
+  if (targetChannel) {
+    targetChannel.send(`**Logged ${client.user.tag} is Online** 🟢`);
+  } else {
+    console.log('لم يتم العثور على الروم المحدد.');
+  }
+}); 
+
+//============================================
