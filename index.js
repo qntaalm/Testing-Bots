3bb081a96bed08f1791b4e49f0b0f615be3cc5ac
qntaalm <askaik000@gmail.com>
2024-08-27 20:00:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -443,7 +443,9 @@ new MessageButton()
 });
 }
 
+let tax;
 if (interaction.customId === 'confirm_price') {
+tax = Math.floor(price * (20) / (19) + (1))
 await interaction.update({
 content: `قم بتحويل المبلغ المطلوب\n\`\`\`C <@${BankId}> ${tax}\`\`\``,
 components: []
